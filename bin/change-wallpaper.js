6f200f6760919772156e6c340422db933d9a4b65
Brady Holt <brady.holt@gmail.com>
2016-06-07 16:59:11 -0500

    Formatting

--- bin/change-wallpaper.js
+++ bin/change-wallpaper.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-"use strict"; 
+"use strict";
 
 const got = require('got');
 const tempfile = require('tempfile');
@@ -11,79 +11,79 @@ const request = require("request")
 const weighted = require('weighted')
 
 function setWallpaper(url) {
-    console.log("Setting wallpaper to: " + url);
-    const file = tempfile(path.extname(url));
-    
-    got
-        .stream(url)
-        .pipe(fs.createWriteStream(file))
-        .on('finish', () => {
-            wallpaper.set(file);
-        });
+  console.log("Setting wallpaper to: " + url);
+  const file = tempfile(path.extname(url));
 
-    //delete temp file (we wait 3 seconds so OSX has time to grab the file and set the wallpaper)
-    setTimeout(fs.unlinkSync, 3000, file)
+  got
+    .stream(url)
+    .pipe(fs.createWriteStream(file))
+    .on('finish', () => {
+      wallpaper.set(file);
+    });
+
+  //delete temp file (we wait 3 seconds so OSX has time to grab the file and set the wallpaper)
+  setTimeout(fs.unlinkSync, 3000, file)
 }
 
 function setBing() {
-    console.log("HANDLER: Bing Photo of the Day");
-    
-    let lastBingPhotoCount = 100;
-    request({
-        url: `http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=${lastBingPhotoCount}&mkt=en-US`,
-        json: true
-    }, function (error, response, body) {
+  console.log("Using Source: Bing Photo of the Day");
 
-        if (!error && response.statusCode === 200) {
-            var randomPhoto = body.images[Math.floor(Math.random()*body.images.length)];
-            setWallpaper("http://www.bing.com" + randomPhoto.url);
-        }
-    });
+  let lastBingPhotoCount = 100;
+  request({
+    url: `http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=${lastBingPhotoCount}&mkt=en-US`,
+    json: true
+  }, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      var randomPhoto = body.images[Math.floor(Math.random() * body.images.length)];
+      setWallpaper("http://www.bing.com" + randomPhoto.url);
+    }
+  });
 }
 
 function setUnsplash() {
-    console.log("HANDLER: Unsplash");
-    
-    var api_key = process.env.UNSPLASH_API_KEY;
-    request({
-        url: "https://api.unsplash.com/photos/random?client_id=" + api_key,
-        json: true
-    }, function (error, response, body) {
+  console.log("Using Source: Unsplash");
 
-        if (!error && response.statusCode === 200) {
-            setWallpaper(body.urls.full);
-        }
-    })
+  var api_key = process.env.UNSPLASH_API_KEY;
+  request({
+    url: "https://api.unsplash.com/photos/random?client_id=" + api_key,
+    json: true
+  }, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      setWallpaper(body.urls.full);
+    }
+  })
 }
 
 function setChromecast() {
-    console.log("HANDLER: Chromecast Backgrounds");
-    
-    request({
-        url: "https://raw.githubusercontent.com/mattburns/chromecast-backgrounds/master/backgrounds.json",
-        json: true
-    }, function (error, response, body) {
+  console.log("Using Source: Chromecast Backgrounds");
 
-        if (!error && response.statusCode === 200) {
-            var randomPhoto = body[Math.floor(Math.random()*body.length)]; 
-            setWallpaper(randomPhoto.url);
-        }
-    });
+  request({
+    url: "https://raw.githubusercontent.com/mattburns/chromecast-backgrounds/master/backgrounds.json",
+    json: true
+  }, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      var randomPhoto = body[Math.floor(Math.random() * body.length)];
+      setWallpaper(randomPhoto.url);
+    }
+  });
 }
 
 function setGooglePhoto() {
-    console.log("HANDLER: Picasa Web Album");
-    
-    request({
-        url: "https://picasaweb.google.com/data/feed/base/user/108862440783718909111/albumid/6217521077777661025?alt=json&kind=photo&max-results=100&hl=en_US&imgmax=1600",
-        json: true
-    }, function (error, response, body) {
+  console.log("Using Source: Picasa Web Album");
 
-        if (!error && response.statusCode === 200) {
-            var randomPhoto = body.feed.entry[Math.floor(Math.random()*body.feed.entry.length)];
-            setWallpaper(randomPhoto.content.src);
-        }
-    });
+  request({
+    url: "https://picasaweb.google.com/data/feed/base/user/108862440783718909111/albumid/6217521077777661025?alt=json&kind=photo&max-results=100&hl=en_US&imgmax=1600",
+    json: true
+  }, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      var randomPhoto = body.feed.entry[Math.floor(Math.random() * body.feed.entry.length)];
+      setWallpaper(randomPhoto.content.src);
+    }
+  });
 }
 
 let wallpaperHandlers = [setBing, setUnsplash, setChromecast, setGooglePhoto]
