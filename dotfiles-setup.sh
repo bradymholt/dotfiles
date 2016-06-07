0b38dc7abe84e405f54de5aa14b208d12090ac2e
Brady Holt <brady.holt@gmail.com>
2016-06-06 22:58:07 -0500

    Add vscode keybindings

--- dotfiles-setup.sh
+++ dotfiles-setup.sh
@@ -38,6 +38,7 @@ echo ""
 echo "STEP 3: symlink external config"
 # VS Code
 ln -sv${LINK_TARGET_EXISTS_HANDLING} "${HOME}/.vscode.settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
+ln -sv${LINK_TARGET_EXISTS_HANDLING} "${HOME}/.vscode.keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
 
 # BetterSnapTool config - The format of the config is complicated so if something ever goes wrong and manual config is needed, here is a screenshot of the important settings: https://cloud.githubusercontent.com/assets/759811/15751225/f76b950c-28ae-11e6-9fd9-7c83aad698b2.png 
 ln -sv${LINK_TARGET_EXISTS_HANDLING} "${HOME}/.BetterSnapTool" "${HOME}/Library/Preferences/com.hegenberg.BetterSnapTool.plist"
