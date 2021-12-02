342bfcda8062c0c7990c64fec5bddde81ac00e9b
Brady Holt <brady.holt@gmail.com>
2021-12-02 13:50:53 -0700

    Backup

--- macos.sh
+++ macos.sh
@@ -2,45 +2,31 @@
 
 # Reference: https://github.com/mathiasbynens/dotfiles/blob/main/.macos
 
+# Set host name
 HOSTNAME=BradyHoltMBP
 sudo scutil --set ComputerName $HOSTNAME
 sudo scutil --set HostName $HOSTNAME
 sudo scutil --set LocalHostName $HOSTNAME
 
-###############################################################################
-# Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
-###############################################################################
-
 # Trackpad: enable tap to click for this user and for the login screen
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-###############################################################################
-# General UI/UX                                                               #
-###############################################################################
-
 # Save to disk (not to iCloud) by default
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
 # Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
+
 # Do not reopen apps at login time
 defaults write com.apple.loginwindow TALLogoutSavesState -bool false
 defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool false
-
-
-###############################################################################
-# Finder                                                                      #
-###############################################################################
 
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
 # Finder: show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-###############################################################################
-# Dock, Dashboard, and hot corners                                            #
-###############################################################################
 
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
