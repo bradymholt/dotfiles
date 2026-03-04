e3abedfe7f87cd0caa2f0509311d0d9d6831cc02
Brady Holt <brady.holt@gmail.com>
2026-03-04 17:10:36 -0600

    Backup

--- link.sh
+++ link.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Move a file/directory from $HOME to ${DOTFILES_PATH} and create a symbolic link in place of the original file
-# Example: # dotfiles/link.sh .tigrc
+# Move a file/directory from $HOME to ${DOTFILES_PATH} (or .secrets with --secret) and create a symbolic link in place of the original file
+# Example: dotfiles/link.sh .tigrc
+# Example: dotfiles/link.sh --secret .ssh/id_rsa
 
 CURRENT_SCRIPT_PATH=$(cd `dirname $0` && pwd -P)
 DOTFILES_PATH=$(cd ${CURRENT_SCRIPT_PATH}/ && pwd)
-FILE_TO_LINK=$HOME/$1
-mv $FILE_TO_LINK $DOTFILES_PATH/
-ln -siv $DOTFILES_PATH/${1} $FILE_TO_LINK
+
+TARGET_DIR=$DOTFILES_PATH
+for arg in "$@"; do
+  if [ "$arg" = "--secret" ]; then
+    TARGET_DIR=$DOTFILES_PATH/.secrets
+  else
+    FILENAME=$arg
+  fi
+done
+
+FILE_TO_LINK=$HOME/$FILENAME
+mv $FILE_TO_LINK $TARGET_DIR/
+ln -siv $TARGET_DIR/${FILENAME} $FILE_TO_LINK
